61abc24d2663c6b05316b726e438686a3f5c53e0
YoussefMobarak1702 <ym17021995@gmail.com>
2025-02-10 12:04:06 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ func main() {
     http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "There's no such thing as a bad day when you're fishing.")
     })
-    fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
+    fmt.Printf("Server running (port=3001), route: http://localhost:3001/helloworld\n")
+    if err := http.ListenAndServe(":3001", nil); err != nil {
         log.Fatal(err)
     }
 }
